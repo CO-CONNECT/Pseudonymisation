fd9518c1128862fac90a34d70d04e991373e43fb
Calum Macdonald <calum.macdonald@sheffield.ac.uk>
2022-01-07 11:03:25 +0000

    switch to columns and use loguru

--- cli/cli.py
+++ cli/cli.py
@@ -1,32 +1,35 @@
 import pandas as pd
 import click
 import os
+import sys
 import hashlib
-import logging
+from loguru import logger
 
-@click.command(help="Command to help pseudonymise data.")
+@click.command(help="Command to pseudonymise csv files, given a salt, the name of the columns to pseudonymise and the input file.")
 @click.option("-s","--salt",help="salt hash",required=True,type=str)
-@click.option("--person-id","-i","--id",help="name of the person_id",required=True,type=str)
+@click.option("columns","--column","-c","--id",help="name of the identifier columns",required=True,type=str,multiple=True)
 @click.option("--output-folder","-o",help="path of the output folder",required=True,type=str)
-@click.option("--chunksize",help="set the chunksize when loading data",type=int,default=None)
+@click.option("--chunksize",help="set the chunksize when loading data, useful for large files",type=int,default=None)
 @click.argument("input",required=True)
-def csv(input,output_folder,chunksize,salt,person_id):
-    logging.info(f"Working on file {input}, pseudonymising column '{person_id}' with salt '{salt}'")
+def csv(input,output_folder,chunksize,salt,columns):
+    columns = list(columns)
+    logger.info(f"Working on file {input}, pseudonymising columns '{columns}' with salt '{salt}'")
 
     #create the dir
     os.makedirs(output_folder,exist_ok=True)
     f_out = f"{output_folder}{os.path.sep}{os.path.basename(input)}"
 
-    logging.info(f"Saving new file to {f_out}")
+    logger.info(f"Saving new file to {f_out}")
 
     #load data
     data = pd.read_csv(input,chunksize=chunksize,dtype=str)
     i = 0 
     while True:
-        data[person_id] =  data[person_id].apply(
-            lambda x: hashlib.sha256(x.encode("UTF-8")).hexdigest()
-        )
-        logging.debug(data[person_id])
+        for col in columns:
+            data[col] =  data[col].apply(
+                lambda x: hashlib.sha256(x.encode("UTF-8")).hexdigest()
+            )
+            logger.debug(data[col])
         
         mode = 'w'
         header=True
@@ -46,11 +49,10 @@ def csv(input,output_folder,chunksize,salt,person_id):
             except StopIteration:
                 break
     
-    logging.info(f"Done with {f_out}!")
+    logger.info(f"Done with {f_out}!")
 
 @click.group()
 def pseudonymise():
-    logging.basicConfig(level=logging.INFO,datefmt='%m/%d/%Y %I:%M:%S %p',format='%(levelname)s  %(asctime)s %(message)s')
     pass
 
 pseudonymise.add_command(csv, "csv")
